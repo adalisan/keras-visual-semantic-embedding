674e58ce0de59965711dfe141f460582236e6c14
Sancar Adali <sadali@bbn.com>
2018-11-30 21:10:12 -0500

    fix model def problems

--- keras_vse/models.py
+++ keras_vse/models.py
@@ -5,43 +5,55 @@ from keras.layers import Convolution2D, Dense, Embedding, GRU, Input,Concatenate
 from keras.models import Model, Sequential
 from keras.applications.vgg19 import VGG19
 from layers import L2Normalize
+from keras.utils import plot_model
+
 from tools import encode_sentences
 
 import pandas
 from translate import Translator
 import pdb
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
 
-def concept_detector(model_file,glove_file, input_length ,data_vocab ,token_count):
+import h5py
+
+def concept_detector(model_file,glove_file, input_length ,data_vocab ,token_count,num_classes):
 
     image_encoder, sentence_encoder, vocab_map , image_feat_extractor = \
         build_pretrained_models(model_file, glove_file,input_length,data_vocab = data_vocab,token_count=token_count)
-    captioned_image_descriptor  = Concatenate(axis=-1)(image_encoder.outputs[0],sentence_encoder.outputs[0])
+    captioned_image_descriptor  = Concatenate(axis=-1)([image_encoder.outputs[0],sentence_encoder.outputs[0]])
 
     concept_detector_scores = Dense(num_classes )(captioned_image_descriptor)
     for enc in image_encoder, sentence_encoder:
         enc.compile(optimizer='nadam', loss='mse')
     end_to_end_model = Model(inputs= [image_feat_extractor.inputs[0] ,sentence_encoder.inputs[0]],
-                             outputs = concept_detector)
+                             outputs = concept_detector_scores)
+    plot_model(end_to_end_model, to_file='model.png')
     return end_to_end_model,vocab_map
 
 
 
 def build_image_feat_extractor():
-    return  VGG19(include_top=False, weights='imagenet', input_tensor=None, input_shape=None, pooling=None)
+    return  VGG19(include_top=False, weights='imagenet', input_tensor=None, input_shape=None, pooling="avg")
 
 def build_image_encoder(weights=None, input_dim=4096, embedding_dim=1024, normalize=True):
-    input = Input(shape=(input_dim,))
-    x = Dense(
+    #input  = Input(shape=(input_dim,))
+    init_model = build_image_feat_extractor()
+    #init_model.compile
+    x= Dense(
         embedding_dim,
         weights=weights
-    )(input)
+    )(init_model.outputs[0])
     if normalize:
         x = L2Normalize()(x)
-    model = Model(input=input, output=x)
-    return model
+    model = Model(input=init_model.inputs[0], output=x)
+    plot_model(model, to_file='image_model.png')
+    return model, init_model
 
 def build_image_encoder_seq_api(weights=None, input_dim=4096, embedding_dim=1024, normalize=True):
-    model = Sequential()
+    model = build_image_feat_extractor()
     
     model.add(Dense(
         embedding_dim,
@@ -73,6 +85,7 @@ def build_sentence_encoder(embedding_weights=None, gru_weights=None, input_lengt
 def build_pretrained_models(model_filename, glove_file,input_length=None,data_vocab = None, token_count=None, normalize=True):
     gru_weights= None
     img_enc_weights = None
+    
     if model_filename is not None:
         img_enc_weights = load_pretrained_parameters(model_filename)
         #, embedding_weights, gru_weights, init_vocab_map = 
@@ -84,8 +97,9 @@ def build_pretrained_models(model_filename, glove_file,input_length=None,data_vo
         glove_embedding_mat= embedding_weights
     glove_embedding_mat,_ = compute_embedding_matrix(glove_file,data_vocab,None)
     vocab_embed_dim = glove_embedding_mat.shape[1]
-    image_feat_extractor = build_image_feat_extractor()
-    image_encoder = build_image_encoder(weights=img_enc_weights, normalize=normalize)
+    
+
+    image_encoder,image_feat_extractor = build_image_encoder(weights=img_enc_weights,  embedding_dim=1024, normalize=normalize)
     print(glove_embedding_mat.shape)
     print(glove_embedding_mat[0,:])
     sentence_encoder = build_sentence_encoder(
@@ -129,30 +143,64 @@ def load_pretrained_embedding_weights(filename):
 
 def compute_embedding_matrix(glove_file,word_index,vocab_embed_dim=None):
     embeddings_index = dict()
-    f= open(glove_file,'r')
-    avg_embed_vec = None
-    num_embeds = 0
-    for line in f:
-        values = line.split(' ')
-        word = values[0]
-        try:
-            coefs = np.asarray(values[1:], dtype='float32')
-        except Exception as e:
-            print (e)
-            print (values[0])
-            print (values[1])
-            print (values[-1])
-            print (line)
-        vocab_embed_dim = len(values[1:])
-        embeddings_index[word] = coefs
-        if avg_embed_vec is None:
-            avg_embed_vec = coefs.copy()
-            
-        else:
-            avg_embed_vec += coefs 
-        num_embeds +=1
-    avg_embed_vec = avg_embed_vec/num_embeds
-    f.close()
+    vocab_embed_dim = 0
+    if glove_file.endswith('pkl') :
+        num_embeds = 0
+        avg_embed_vec = None
+        embeddings_index=pickle.load(open(glove_file,'rb'))
+        for _,avec in embeddings_index.items():
+            vocab_embed_dim  = avec.size
+            if avg_embed_vec is None:
+                avg_embed_vec = avec.copy()
+            else:
+                avg_embed_vec += avec 
+            num_embeds +=1
+        avg_embed_vec = avg_embed_vec/num_embeds
+    elif glove_file.endswith('h5') :
+        glove_hdf5 = h5py.File(glove_file,'r')
+        embeddings_index = glove_hdf5["embed_dict"]
+        avg_embed_vec = embeddings_index["avg_word_vector"].copy()
+        vocab_embed_dim = avg_embed_vec.size
+        del embeddings_index["avg_word_vector"]
+        
+
+    else:
+        f= open(glove_file,'r')
+        avg_embed_vec = None
+        num_embeds = 0
+        for line in f:
+            values = line.split(' ')
+            word = values[0]
+            try:
+                coefs = np.asarray(values[1:], dtype='float32')
+            except Exception as e:
+                print (e)
+                print (values[0])
+                print (values[1])
+                print (values[-1])
+                print (line)
+            vocab_embed_dim = len(values[1:])
+            embeddings_index[word] = coefs
+            if avg_embed_vec is None:
+                avg_embed_vec = coefs.copy()
+                
+            else:
+                avg_embed_vec += coefs 
+            num_embeds +=1
+        avg_embed_vec = avg_embed_vec/num_embeds
+
+        f.close()
+        f= open(glove_file+'.pkl','wb')
+        pickle.dump(embeddings_index, f)
+        f.close()
+        with h5py.File(glove_file+'.h5','w') as f:
+            embed_dict=f.create_group("embed_dict")
+            for word, i in word_index.items():
+                embed_dict[word] = coefs
+            embed_dict["avg_word_vector"] = avg_embed_vec
+        
+        
+    vocab_embed_dim
     embedding_matrix = np.zeros((len(word_index) + 1, vocab_embed_dim))
     for word, i in word_index.items():
         embedding_vector = embeddings_index.get(word)
