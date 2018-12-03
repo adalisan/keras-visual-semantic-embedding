9170f9e5043bed4b06a0885672477ce0a921081a
Sancar Adali <sadali@bbn.com>
2018-12-03 14:04:00 -0500

    add script for downloading pretrained (theano) models and corresponding dictionaries.

---
+++ pretrained_model/download_pretrained_models.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+wget http://www.cs.toronto.edu/~rkiros/datasets/f8k.zip
+wget http://www.cs.toronto.edu/~rkiros/datasets/f30k.zip
+wget http://www.cs.toronto.edu/~rkiros/datasets/coco.zip
+wget http://www.cs.toronto.edu/~rkiros/models/vse.zip
