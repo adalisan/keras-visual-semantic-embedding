a8039c6f163275b52040561ff888bf45a76118df
Sancar Adali <sadali@bbn.com>
2018-12-05 18:27:17 -0500

    set gpu mode for main script

--- keras_vse/main.py
+++ keras_vse/main.py
@@ -11,6 +11,13 @@ from keras_image_caption_data_generator import MultimodalInputDataGenerator as d
 from models import concept_detector
 from keras import backend  as K
 
+import tensorflow as tf
+
+
+if 'tensorflow' == K.backend():
+
+    from keras.backend.tensorflow_backend import set_session
+
 def mil_squared_error(y_true, y_pred):
     return K.tile(K.square(K.max(y_pred) - K.max(y_true)), 16)
 
@@ -25,11 +32,23 @@ if __name__ == '__main__':
       default="/nfs/mercury-11/u113/projects/AIDA/glove.840B.300d.txt" , type=str)
     parser.add_argument('--length', type=int, default=None)
     parser.add_argument('--maxtokencount', type=int, default=32198)
+    parser.add_argument('--fix_gpu', type=int, default=-1)
     
     args = parser.parse_args()
 
     KERAS_DATAGEN_DIR = "/nfs/mercury-11/u113/projects/AIDA/GoogleImageDownload_Rus_Scenario/image_data_links"
-
+    gpu_id = 3
+    gpu_id_str = str(int(gpu_id)) 
+    if args.fix_gpu >= 0:
+        print ("Overwriting gpu id from cfg file with given arg {}".format(args.fix_gpu))
+        gpu_id_str = str(args.fix_gpu)
+    #%% 
+    
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    config.gpu_options.visible_device_list = gpu_id_str
+    #session = tf.Session(config=config)
+    set_session(tf.Session(config=config))
     train_df = pd.read_csv(args.train_csv_file, encoding='utf8')
     print( train_df.apply(lambda x: pd.lib.infer_dtype(x.values)))
     texts = train_df["image_captions"].values.tolist()
@@ -54,7 +73,7 @@ if __name__ == '__main__':
 
 
     train_df =pd.read_csv(args.train_csv_file)
-    train_datagen = datagen()
+    train_datagen = datagen(width_shift_range = 0.2,zoom_range=0.2,rotation_range=25, height_shift_range=0.3 )
     train_data_it = train_datagen.flow_from_dataframe( 
                             dataframe= train_df,
                             directory= None,
